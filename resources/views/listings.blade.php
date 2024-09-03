c63ba954606ee507e825d1b7238e4868825e2d65
simon <simonwubshet254@gmail.com>
2024-09-03 22:12:26 +0300

    setup

---
+++ resources/views/listings.blade.php
@@ -0,0 +1,21 @@
+<h1>{{$heading}}</h1>
+@unless (count ($listings) == 0)
+
+@foreach($listings as $listing)
+<h2>
+<a href="/listings/{{$listing['id']}}">{{$listing['title']}}</a>
+</h2>
+<p>
+{{$listing['description']}}
+</p>
+<p>
+    {{$listing['id']}}
+    </p>
+@endforeach
+
+@else
+<p>No Listings founde/p>
+@endunless
+
+
+<h1>{{$heading}}</h1>
