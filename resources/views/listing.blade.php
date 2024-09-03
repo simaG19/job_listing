01e2b702fd12bffcecc7ca3bd71f96a5629964d6
simon <simonwubshet254@gmail.com>
2024-09-03 23:12:32 +0300

    integrate to listing page

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -3,7 +3,9 @@
 @extends('layout')
 
 @section('content')
-
+<h2>
+    {{$listing['title']}}
+    </h2>
     <p>
     {{$listing['description']}}
     </p>
--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -2,6 +2,8 @@
 @extends('layout')
 
 @section('content')
+@include('partials._hero')
+
 <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
 @unless (count ($listings) == 0)
 
