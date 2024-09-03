ba6142b824d7827436fd8c6a075c5544f5a67fe4
simon <simonwubshet254@gmail.com>
2024-09-03 23:04:54 +0300

    logo image

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -1,9 +1,14 @@
-<h2>
-    {{$listing['title']}}
-    </h2>
+
+
+@extends('layout')
+
+@section('content')
+
     <p>
     {{$listing['description']}}
     </p>
     <p>
         {{$listing['id']}}
         </p>
+
+@endsection
